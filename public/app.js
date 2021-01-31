3db7cb111c1f26bba4df3a34a8f6a8c42d681901
Dan7n <62748054+Dan7n@users.noreply.github.com>
2021-01-31 15:16:48 +0100

    organized code in app.js

--- public/app.js
+++ public/app.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
 
-    //check if darkmode is enabled
-    const localStorageCheck = localStorage.getItem("darkMode")
-    if (localStorageCheck && localStorageCheck !== null) {
-        localStorageCheck === "false" ? $("#toggle").prop("checked", true) : $("#toggle").prop("checked", false)
-    }
-
+    //variables ----------------
     const hourOfDay = new Date().getHours();
     const welcome = $(".welcome");
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; //using this to print out todays date
@@ -13,9 +8,27 @@ $(document).ready(function() {
     const $ulElement = $("#ulElement")
     const ulChildren = $("ul").children().length;
 
+
+    //check if darkmode is enabled
+    const localStorageCheck = localStorage.getItem("darkMode")
+    if (localStorageCheck && localStorageCheck !== null) {
+        console.log(localStorageCheck)
+        if (localStorageCheck === "false") {
+            toggleNightModeOn()
+            $("#toggle").prop("checked", true)
+
+        } else if (localStorageCheck === "true") {
+            toggleNightModeOff()
+            $("#toggle").prop("checked", false)
+        }
+    }
+
+    //display a live clock and date of year
     $(".time").html(getTime())
     $(".date").html(`${whatDayIsIt}, ${getDate()}`)
 
+
+    //if no todos left in todo list
     if (ulChildren <= 0) {
         const $header = $("<h1>").text("Wohoo! Looks like it's time to sit back and relax for now!").fadeIn(300)
         const $newImg = $("<img>").attr("src", "./../assets/chillin.svg").attr("alt", "A relaxed cat").fadeIn(300)
@@ -24,38 +37,23 @@ $(document).ready(function() {
         if ($ulElement.has("li") && $ulElement.has("h1")) {
             $ulElement.remove($header).remove($newImg)
         }
-    
     } 
 
 
     //print out personalized welcome message based on the time of day
     sayHi(hourOfDay, welcome)
 
+    //live clock
     setInterval(function(){
         $(".time").html(getTime())
     }, 1000);
 
     //darkmode toggle
-    $("#toggle").on("click", function() {
-        if (!$(this).prop("checked")) {
-
-            document.body.style.setProperty("--gradient-from", "#394265")
-            document.body.style.setProperty("--gradient-to", "#928DAB")
-            document.body.style.setProperty("--stroke-color", "#394265")
-            document.body.style.setProperty("--ul-bg-main", "hsl(227.7,27.8%,29%)")
-            document.body.style.setProperty("--font-color-main", "#f5d1ee")
-            window.localStorage.setItem("darkMode", "true")
-        } else {
-            document.body.style.setProperty("--gradient-from", "#ece9e6")
-            document.body.style.setProperty("--gradient-to", "#ffffff")
-            document.body.style.setProperty("--stroke-color", "#fee440")
-            document.body.style.setProperty("--ul-bg-main", "#f4f5f5")
-            document.body.style.setProperty("--font-color-main", "#202147")
-            window.localStorage.setItem("darkMode", "false")
-                }
-    })
+    $("#toggle").on("click", toggleNightMode)
 })
-// -------- functions
+
+
+// * --------------- functions ---------------
 
 const sayHi = (time, element) => {
     if (time >= 0 && time < 12) {
@@ -69,19 +67,54 @@ const sayHi = (time, element) => {
     }
 }
 
-
 function getTime() {
     let hours = new Date().getHours();
     let minutes = new Date().getMinutes();
     let seconds = new Date().getSeconds();
-
     hours = (hours < 10) ? "0" + hours : hours;
     minutes = (minutes < 10) ? "0" + minutes : minutes;
     seconds = (seconds < 10) ? "0" + seconds : seconds;
-    
     return `${hours}:${minutes}:${seconds}`;
 }
 
 function getDate() {
     return `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`;
+}
+
+
+// !TODO fix this:
+function toggleNightMode() {
+    if (!$(this).prop("checked")) {
+        document.body.style.setProperty("--gradient-from", "#394265")
+        document.body.style.setProperty("--gradient-to", "#928DAB")
+        document.body.style.setProperty("--stroke-color", "#394265")
+        document.body.style.setProperty("--ul-bg-main", "hsl(227.7,27.8%,29%)")
+        document.body.style.setProperty("--font-color-main", "#f5d1ee")
+        window.localStorage.setItem("darkMode", "true")
+    } else {
+        document.body.style.setProperty("--gradient-from", "#ece9e6")
+        document.body.style.setProperty("--gradient-to", "#ffffff")
+        document.body.style.setProperty("--stroke-color", "#fee440")
+        document.body.style.setProperty("--ul-bg-main", "#f4f5f5")
+        document.body.style.setProperty("--font-color-main", "#202147")
+        window.localStorage.setItem("darkMode", "false")
+            }
+}
+
+function toggleNightModeOn() {
+    document.body.style.setProperty("--gradient-from", "#394265")
+    document.body.style.setProperty("--gradient-to", "#928DAB")
+    document.body.style.setProperty("--stroke-color", "#394265")
+    document.body.style.setProperty("--ul-bg-main", "hsl(227.7,27.8%,29%)")
+    document.body.style.setProperty("--font-color-main", "#f5d1ee")
+    window.localStorage.setItem("darkMode", "true")
+}
+
+function toggleNightModeOff() {
+    document.body.style.setProperty("--gradient-from", "#394265")
+    document.body.style.setProperty("--gradient-to", "#928DAB")
+    document.body.style.setProperty("--stroke-color", "#394265")
+    document.body.style.setProperty("--ul-bg-main", "hsl(227.7,27.8%,29%)")
+    document.body.style.setProperty("--font-color-main", "#f5d1ee")
+    window.localStorage.setItem("darkMode", "true")
 }

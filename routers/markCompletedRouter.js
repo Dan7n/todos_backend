00b66a405f4e07b0c45fa262b22e81e77636ce02
Dan7n <62748054+Dan7n@users.noreply.github.com>
2021-02-06 20:31:51 +0100

    fixed problem where marking a task complete caused website to redirect to the first page instead of the page the marked item was on

--- routers/markCompletedRouter.js
+++ routers/markCompletedRouter.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const markCompletedRouter = express.Router();
 const Todo = require("../models/Todos");
+const paginationMiddleware = require("./paginationMiddleware");
 
-markCompletedRouter.get("/:id", async (req, res) => {
-  const checkedElement = await Todo.findOne({ _id: req.params.id });
-  if (checkedElement.checked === false) {
-    await Todo.updateOne({ _id: req.params.id }, { checked: "true" });
-  } else {
-    await Todo.updateOne({ _id: req.params.id }, { checked: "false" });
+markCompletedRouter.get(
+  "/:id",
+  paginationMiddleware(Todo),
+  async (req, res) => {
+    const checkedElement = await Todo.findOne({ _id: req.params.id });
+    if (checkedElement.checked === false) {
+      await Todo.updateOne({ _id: req.params.id }, { checked: "true" });
+    } else {
+      await Todo.updateOne({ _id: req.params.id }, { checked: "false" });
+    }
+    res.redirect(
+      `/?page=${req.headers.page}&limit=${req.headers.limit}&sort=${req.headers.sort}`
+    );
   }
-  res.redirect("/");
-});
+);
 
 module.exports = markCompletedRouter;
